0c9bc0a6b5df90fbc4b47e1388362d840d6524c7
harlie.levine@gmail.com <harlie.levine@gmail.com>
2012-03-07 16:27:58 -0600

    intermediary commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,28 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  
+  
+  
+ 
+  
+  
+  def self.find_foursq_user(token)
+    #fetch data from foursq
+    #get user_id, email, name
+    #find user by email
+    #if exists return auth token
+    #else create new user
+    
+    foursquare = Foursquare::Base.new(token)
+    
+    if user = User.where(:email => email]).first
+
+    else
+     
+    end
+    return user
+  end
+  
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,15 @@ Adventeur::Application.routes.draw do
     root :to => 'home#index'
   end
   root :to => "home#index"
+  namespace :api do
+    namespace :v1 do
+      resources :registrations, :only => :create
+    end
+  end
   devise_for :users
   resources :users, :only => :show
- resources :adventures
+  resources :adventures
+
+  
+  
 end
