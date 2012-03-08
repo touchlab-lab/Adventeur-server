37ee033c85c8fcd1b01fdd8d27f0df3b69f729b5
harlie.levine@gmail.com <harlie.levine@gmail.com>
2012-03-08 17:23:31 -0600

    removing weird characters from seed

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -3,12 +3,12 @@ class Api::V1::SearchController < ActionController::Base
   respond_to :json
   
   def nearest
-    @checkpoints = Checkpoint.start.near( [params["search"]["lat"].to_f,params["search"]["lng"].to_f],params["search"]["radius"] )
+    @checkpoints = Checkpoint.start.near( [params["search"]["lat"].to_f,params["search"]["lng"].to_f],params["search"]["radius"] ).limit(3)
     render 'api/v1/search'
   end
   
   def intersect
-    @checkpoints = Checkpoint.where(:venue_id => params["search"]["venue_id"]).limit(10)
+    @checkpoints = Checkpoint.where(:venue_id => params["search"]["venue_id"]).limit(3)
     render 'api/v1/search'
     
   end
