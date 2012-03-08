d68d207af42864f6edef79fa364cdcc7ea8cd537
harlie.levine@gmail.com <harlie.levine@gmail.com>
2012-03-08 17:51:11 -0600

    we don't need a first user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts 'SETTING UP DEFAULT USER LOGIN'
-user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
-puts 'New user created: ' << user.name
+
+#puts 'SETTING UP DEFAULT USER LOGIN'
+#user = User.create! :name => 'First User', :email => 'user@example.com', :password => 'please', :password_confirmation => 'please'
+#puts 'New user created: ' << user.name
 
 Adventure.delete_all
 Venue.delete_all
