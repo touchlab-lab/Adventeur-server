5890829a49ae7da7b90b0adaf39718869908d4c5
harlie.levine@gmail.com <harlie.levine@gmail.com>
2012-03-08 19:20:36 -0600

    fixing bad seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,16 +20,17 @@ Checkpoint.delete_all
 
 #FakeWeb.allow_net_connect = false
 venues = {}
-["4bded100921d952185016932", "4b05866ef964a520e16122e3", "4b058660f964a520465f22e3", "4b37d793f964a520454725e3", "4ad4c000f964a5208ceb20e3", 
-  "440fee99f964a520b2301fe3", "41536380f964a5201f1d", "47e2990df964a520434e1fe3",  "40b13b00f964a520aaf31ee3", "43e5cda9f964a520f92e1fe3",
-  "41536380f964a5201f1d", "40b13b00f964a52083f31ee3", "4a5684e3f964a52051b51fe3", "43598100f964a520eb281fe3", "47e2990df964a520434e1fe3",
-  "45f19148f964a520cf431fe3", "43598100f964a520eb281fe3", "40fb0f00f964a520fc0a1fe3", "45fbfa6df964a5206c441fe3", "4b255d5bf964a520b47024e3", 
+[ "4bded100921d952185016932", "4b05866ef964a520e16122e3", "4b058660f964a520465f22e3", "4b37d793f964a520454725e3", "4ad4c000f964a5208ceb20e3", 
+  "440fee99f964a520b2301fe3", "41536380f964a5201f1d1fe3", "47e2990df964a520434e1fe3", "40b13b00f964a520aaf31ee3", "43e5cda9f964a520f92e1fe3",
+  "40b13b00f964a52083f31ee3", "4a5684e3f964a52051b51fe3", "43598100f964a520eb281fe3",
+  "45f19148f964a520cf431fe3", "40fb0f00f964a520fc0a1fe3", "45fbfa6df964a5206c441fe3", "4b255d5bf964a520b47024e3", 
   "4b253b76f964a5206b6e24e3", "4beb1a5062c0c92811b4e1d4", "4ac8fc7df964a520c3bd20e3", "4ac900a1f964a520ecbd20e3"
   ].each do |id|
  # FakeWeb.register_uri(:get, "https://api.foursquare.com/v2/venues/#{id}?client_id=#{Adventeur::Application.config.foursquare_id}&client_secret=#{Adventeur::Application.config.foursquare_secret}",
   #                 :response => File.expand_path("spec/faker/venues/#{id}.json"))
   
   venues[id] = Venue.fetch_and_add!(id)
+  puts "seeded #{id}"
 end
 
 # Startup Bus adventure
@@ -43,14 +44,14 @@ adventure.checkpoints.create!({:tout => "tout san", :description => "desc", :ven
 # A Perfect Game of Foosball
 adventure3 = Adventure.create! :name => 'A Perfect Game of Foosball', :description => 'desc'
 adventure3.checkpoints.create!({:tout => "Loud music in a dark place.", :description => "Our first stop was the Parish, where Slash was supposed to play later that evening.  Hung out in the corner booth anticipating the show.", :venue_id => venues['440fee99f964a520b2301fe3'].id, :latitude => venues['440fee99f964a520b2301fe3'].latitude, :longitude => venues['440fee99f964a520b2301fe3'].longitude })
-adventure3.checkpoints.create!({:tout => "Longtime Austinites remember when the Red Eyed Fly was home to Wilco.", :description => "The Red Eyed Fly was always famous for discovering musicians before they got big. Keep an eye on whoever you see play there.", :venue_id => venues['41536380f964a5201f1d'].id, :latitude => venues['41536380f964a5201f1d'].latitude, :longitude => venues['41536380f964a5201f1d'].longitude })
+adventure3.checkpoints.create!({:tout => "Longtime Austinites remember when the Red Eyed Fly was home to Wilco.", :description => "The Red Eyed Fly was always famous for discovering musicians before they got big. Keep an eye on whoever you see play there.", :venue_id => venues['41536380f964a5201f1d1fe3'].id, :latitude => venues['41536380f964a5201f1d1fe3'].latitude, :longitude => venues['41536380f964a5201f1d1fe3'].longitude })
 adventure3.checkpoints.create!({:tout => "If I could be at the Boiling Pot right now, I'd be happy.", :description => "Seafood at it's messiest. Delicious cajun mix of crab, shrimp, sausage corn and potatoes dumped right on the table and ready to eat.", :venue_id => venues['47e2990df964a520434e1fe3'].id, :latitude => venues['47e2990df964a520434e1fe3'].latitude, :longitude => venues['47e2990df964a520434e1fe3'].longitude })
 adventure3.checkpoints.create!({:tout => "Preacher's collar with an IPA chaser", :description => "You could come here every day for 4 months and never drink the same beer twice.", :venue_id => venues['40b13b00f964a520aaf31ee3'].id, :latitude => venues['40b13b00f964a520aaf31ee3'].latitude, :longitude => venues['40b13b00f964a520aaf31ee3'].longitude })
 adventure3.checkpoints.create!({:tout => "Rack 'em, shoot 'em, sink 'em.", :description => "We learned there's no such thing as a friendly game, and the fooz is brutal.  Live by the ball, die by the ball.", :venue_id => venues['43e5cda9f964a520f92e1fe3'].id, :latitude => venues['43e5cda9f964a520f92e1fe3'].latitude, :longitude => venues['43e5cda9f964a520f92e1fe3'].longitude })
 
 # My Accidental Karaoke
 adventure4 = Adventure.create! :name => 'My Accidental Karaoke', :description => 'desc'
-adventure4.checkpoints.create!({:tout => "Longtime Austinites remember when the Red Eyed Fly was home to Wilco.", :description => "The Red Eyed Fly was always famous for discovering musicians before they got big. Keep an eye on whoever you see play there.", :venue_id => venues['41536380f964a5201f1d'].id, :latitude => venues['41536380f964a5201f1d'].latitude, :longitude => venues['41536380f964a5201f1d'].longitude })
+adventure4.checkpoints.create!({:tout => "Longtime Austinites remember when the Red Eyed Fly was home to Wilco.", :description => "The Red Eyed Fly was always famous for discovering musicians before they got big. Keep an eye on whoever you see play there.", :venue_id => venues['41536380f964a5201f1d1fe3'].id, :latitude => venues['41536380f964a5201f1d1fe3'].latitude, :longitude => venues['41536380f964a5201f1d1fe3'].longitude })
 adventure4.checkpoints.create!({:tout => "The long tail of swank.", :description => "Open for 100 years with the finest patios in town.  Everyone comes through here eventually.  Grab a seat at a bar and let the world revolve around you.", :venue_id => venues['40b13b00f964a52083f31ee3'].id, :latitude => venues['40b13b00f964a52083f31ee3'].latitude, :longitude => venues['40b13b00f964a52083f31ee3'].longitude })
 adventure4.checkpoints.create!({:tout => "Stay for a while.", :description => "Lamb and goat burger, inventive art. Love the small french feel hidden away on the other side of the freeway.", :venue_id => venues['4a5684e3f964a52051b51fe3'].id, :latitude => venues['4a5684e3f964a52051b51fe3'].latitude, :longitude => venues['4a5684e3f964a52051b51fe3'].longitude })
 adventure4.checkpoints.create!({:tout => "Beer-battered asparagus. Yes.", :description => "Warm atmosphere, southern food and strong drinks make this place perfect for any occasion.", :venue_id => venues['43598100f964a520eb281fe3'].id, :latitude => venues['43598100f964a520eb281fe3'].latitude, :longitude => venues['43598100f964a520eb281fe3'].longitude })
